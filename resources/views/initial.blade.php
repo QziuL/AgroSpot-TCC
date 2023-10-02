48f14f309bc58413338927188b47b359ad68d9cb
QziuL <lf.quinholi2@gmail.com>
2023-10-02 20:21:28 -0300

    código inicial

---
+++ resources/views/initial.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="/css/style.css">
+
+    <title>AgroSpot</title>
+</head>
+<body>
+    <nav>
+        <a href="">Logo</a>
+        <a href="">Login agricultor</a>
+        <a href="">Opções</a>
+    </nav>
+    <main>
+        <input type="text" name="" id="" placeholder="Busque seus produtos...">
+        <p>Conecte-se ao campo, transforme sua alimentação.</p>
+    </main>
+    <footer>
+        <a href="">Insta</a>
+        <a href="">Email</a>
+        <div>
+            <p>Entre em contato</p>
+            <p>(xx) xxxx-xxxx</p>
+        </div>
+    </footer>
+</body>
+</html>
