554bfde323a0ce5faeb41a1641d9ccbbba4545b6
marcelly <marcelly.quarenta@gmail.com>
2023-10-12 15:59:02 -0300

    CRIAÇÃO DA TELA INICIAL

--- resources/views/initial.blade.php
+++ resources/views/initial.blade.php
@@ -10,21 +10,34 @@
     <title>AgroSpot</title>
 </head>
 <body>
-    <nav>
-        <a href="">Logo</a>
-        <a href="">Login agricultor</a>
-        <a href="">Opções</a>
+    <nav id="navbar">
+        <a href="" id="navbar-logo">Logo</a>
+        <div id="navbar-menu">
+            <ul>
+                <li><a href="" id="menu-login">Login</a></li>
+                <li class="menu-opcoes-dropdown">
+                    <a href="" id="menu-opcoes">Opções</a>
+                    <div class="menu-opcoes-dropdown-content">
+                        <a href="">Cadastrar</a>
+                        <a href="">Vincular perfil a uma feira</a>
+                        <a href="">Ver mapa</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
     </nav>
     <main>
-        <input type="text" name="" id="" placeholder="Busque seus produtos...">
-        <p>Conecte-se ao campo, transforme sua alimentação.</p>
+        {{-- <input type="text" name="" id="" placeholder="Busque seus produtos..."> --}}
+        <p id="frase-efeito">Conecte-se ao campo, transforme sua alimentação.</p>
     </main>
     <footer>
-        <a href="">Insta</a>
-        <a href="">Email</a>
         <div>
-            <p>Entre em contato</p>
-            <p>(xx) xxxx-xxxx</p>
+            <a href="">Insta</a>
+            <a href="">Email</a>
+            <div>
+                <p>Entre em contato</p>
+                <p>(xx) xxxx-xxxx</p>
+            </div>
         </div>
     </footer>
 </body>
