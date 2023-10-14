7392a2d0517f002d1693c329ebf4a97c752244f5
QziuL <lf.quinholi2@gmail.com>
2023-10-14 20:18:39 -0300

    tela de login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,3 +1,31 @@
-@extends('layouts.main')
-
-@section('title', 'AgroSpot')
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <link rel="stylesheet" href="/css/loginStyle.css">
+    
+    <title>AgroSpot - Login</title>
+</head>
+<body>
+    <div class="background"></div>
+    <div id="div-login">
+        <h1>Login</h1>
+        <div id="div-form">
+            <form action="">
+                <label for="email">E-mail</label>
+                <input type="email" name="email" id="email" placeholder="Digite seu e-mail...">
+                <label for="email">Senha</label>
+                <input type="password" name="password" id="password" placeholder="Digite sua senha...">
+                <input type="submit" value="Login" id="input-login">
+            </form>
+        </div>
+        <div id="div-cadastro">
+            <p>Não tem cadastro?</p>
+            <a href="#">Cadastrar</a>
+        </div>
+    </div>
+</body>
+</html>
