0a6099d80e1da33323282c29a47daab200db5424
QziuL <lf.quinholi2@gmail.com>
2023-10-17 23:06:53 -0300

    uso das controllers nas rotas

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Http\Controllers\EventController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [EventController::class, 'viewIndex'])->name('index');
 
 Route::get('/login', [EventController::class, 'viewLogin'])->name('login.view');
 
-Route::get('/register', [EventController::class, 'viewRegister'])->name('register.view');
+Route::get('/register', [RegisterController::class, 'viewRegister'])->name('register.view');
+Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
