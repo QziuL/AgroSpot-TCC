69887dd2537f312aa122f332b01860f97591f5ec
QziuL <lf.quinholi2@gmail.com>
2023-10-19 17:30:22 -0300

    const para colunas da tabela

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Produto extends Model
 {
     use HasFactory;
+
+    public const TABELA_PRODUTO = "produtos";
+
+    public const CAMPO_DELETED_AT = "deleted_at";
+    public const CAMPO_CREATED_AT = "created_at";
+    public const CAMPO_UPDATED_AT = "updated_at";
+
+    public const CAMPO_ID = "id";
+    public const CAMPO_NOME = "nome";
+    public const CAMPO_DESCRICAO = "descricao";
+    public const CAMPO_CODIGO = "codigo";
+    public const CAMPO_DISPONIBILIDADE = "disponibilidade";
 }
