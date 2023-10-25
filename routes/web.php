2f40a80064cdc71de997fcef79d9204810ef95e6
QziuL <lf.quinholi2@gmail.com>
2023-10-25 17:34:44 -0300

    rota login auth/logout e ajuste no controller

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,16 @@
 <?php
 
 use App\Http\Controllers\EventController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ProdutoController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [EventController::class, 'viewIndex'])->name('index');
 
-Route::get('/login', [EventController::class, 'viewLogin'])->name('login.view');
+Route::get('/login', [LoginController::class, 'index'])->name('login.view');
+Route::post('/login/auth', [LoginController::class, 'auth'])->name('login.auth');
+Route::post('/logout', [LoginController::class, 'logout'])->name('login.logout');
 
 Route::get('/register', [RegisterController::class, 'viewRegister'])->name('register.view');
 Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
