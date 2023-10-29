71c03a3e9c5a0610537f4a2dbd1517a0fab75f3f
QziuL <lf.quinholi2@gmail.com>
2023-10-29 20:20:29 -0300

    finalizado cadastro de usuário

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -15,29 +15,40 @@ class RegisterController extends Controller
     public function store(Request $request) {
         $usuario = new User();
 
-        $usuario->name = mb_strtoupper($request->name, 'UTF-8');
-        $usuario->email = $request->email;
-        $usuario->password = bcrypt($request->password);
-        $usuario->phone = $request->phone;
-        $usuario->agricultor = intval($request->agricultor);
-        $usuario->cep = $request->cep;
-        $usuario->cidade = $request->city;
-        $usuario->nome_propriedade = $request->nomePropriedade;
-
-        if($request->agricultor == 0){ $usuario->agricultor = $request->agricultor; }
+        $usuario->name              = mb_strtoupper($request->name, 'UTF-8');
+        $usuario->email             = $request->email;
+        $usuario->password          = bcrypt($request->password);
+        $usuario->phone             = $request->phone;
+        $usuario->agricultor        = intval($request->button_radio);     //boolval($request->button_radio);
+        
+        if($usuario->agricultor == 0)
+        {
+            $usuario->cep               = null;
+            $usuario->cidade            = null;
+            $usuario->nome_propriedade  = null;
+        }
         else
         {
-            $usuario->agricultor = $request->agricultor;
-            $usuario->cep = $request->cep;
-            $usuario->cidade = $request->city;
-            $usuario->nome_propriedade = $request->nomePropriedade;
+            $usuario->cep               = $request->cep;
+            $usuario->cidade            = mb_strtoupper($request->city, 'UTF-8');
+            $usuario->nome_propriedade  = mb_strtoupper($request->nomePropriedade, 'UTF-8');
         }
-
-
+        
+        
 
         $usuario->save();
 
-        return redirect('/')->with('msg', 'Sucesso ao cadastrar!');
-        // dd($request->all());
+        return redirect()->route('index')->with('msg', 'Sucesso ao cadastrar!');
+
+        // if($usuario->agricultor == 0){ $usuario->agricultor = intval($request->button_radio); }
+        // else
+        // {
+        //     $usuario->agricultor        = $request->agricultor;
+        //     $usuario->cep               = $request->cep;
+        //     $usuario->cidade            = $request->city;
+        //     $usuario->nome_propriedade  = $request->nomePropriedade;
+        // }
+       
+        //dd($usuario);
     }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->boolean('agricultor')->default('0')->nullable();
+            $table->integer('agricultor')->default(0);
             $table->string('phone', 15);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
