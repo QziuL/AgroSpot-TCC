30a7506e7f99a3925e2e95cc9570aa827e46d828
QziuL <lf.quinholi2@gmail.com>
2023-10-28 23:09:30 -0300

    método store para cadastro user

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
@@ -12,6 +13,31 @@ class RegisterController extends Controller
     }
 
     public function store(Request $request) {
-        dd($request->all());
+        $usuario = new User();
+
+        $usuario->name = mb_strtoupper($request->name, 'UTF-8');
+        $usuario->email = $request->email;
+        $usuario->password = bcrypt($request->password);
+        $usuario->phone = $request->phone;
+        $usuario->agricultor = intval($request->agricultor);
+        $usuario->cep = $request->cep;
+        $usuario->cidade = $request->city;
+        $usuario->nome_propriedade = $request->nomePropriedade;
+
+        if($request->agricultor == 0){ $usuario->agricultor = $request->agricultor; }
+        else
+        {
+            $usuario->agricultor = $request->agricultor;
+            $usuario->cep = $request->cep;
+            $usuario->cidade = $request->city;
+            $usuario->nome_propriedade = $request->nomePropriedade;
+        }
+
+
+
+        $usuario->save();
+
+        return redirect('/')->with('msg', 'Sucesso ao cadastrar!');
+        // dd($request->all());
     }
 }
