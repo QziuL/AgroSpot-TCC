bb99a39826482bba8b03e4501fa9bc32cd971c03
QziuL <lf.quinholi2@gmail.com>
2023-11-12 23:11:55 -0300

    controller cadastro agricultor [indefinido]

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -13,6 +13,10 @@ class RegisterController extends Controller
         return view('register');
     }
 
+    public function viewRegisterAgricultor() {
+        return view('registerAgricultor');
+    }
+
     public function store(Request $request) {
         
         // 0 == usuario comum
@@ -28,23 +32,39 @@ class RegisterController extends Controller
             
             $usuario->save();
 
-            return redirect()->route('login.view')->with('msg', 'Sucesso ao cadastrar!');
+            return redirect()->route('login')->with('msg', 'Sucesso ao cadastrar!');
         }
         else
         {
-            $agricultor = new Agricultor();
-            $agricultor->cpf                = $request->cpf;
-            $agricultor->name               = mb_strtoupper($request->name, 'UTF-8');
-            $agricultor->phone              = $request->phone;
-            $agricultor->password           = bcrypt($request->password);
-            $agricultor->email              = $request->email;
-            $agricultor->cep                = $request->cep;
-            $agricultor->cidade             = mb_strtoupper($request->city, 'UTF-8');
-            $agricultor->nome_propriedade   = mb_strtoupper($request->nomePropriedade, 'UTF-8');
+            // $agricultor = new Agricultor();
+            // $agricultor->cpf                = $request->cpf;
+            // $agricultor->name               = mb_strtoupper($request->name, 'UTF-8');
+            // $agricultor->phone              = $request->phone;
+            // $agricultor->password           = bcrypt($request->password);
+            // $agricultor->email              = $request->email;
+            // $agricultor->cep                = $request->cep;
+            // $agricultor->cidade             = mb_strtoupper($request->city, 'UTF-8');
+            // $agricultor->nome_propriedade   = mb_strtoupper($request->nomePropriedade, 'UTF-8');
 
-            $agricultor->save();
+            // $agricultor->save();
 
-            return redirect()->route('login.index')->with('msg', 'Sucesso ao cadastrar!');
-        }   
+            $usuario = new User();
+            $usuario->name              = mb_strtoupper($request->name, 'UTF-8');
+            $usuario->phone             = $request->phone;
+            $usuario->email             = $request->email;
+            $usuario->password          = bcrypt($request->password);
+            
+            $usuario->save();
+
+            // $email = $usuario->email;
+            // $user_id = User::where('email', $email)->first()->id;
+
+            return redirect()->route('register.agricultor')->with('msg', 'Sucesso ao cadastrar!');
+        }
+    }
+
+    public function storeAgricultor(Request $request) {
+        $user_id = User::first()->id;
+        dd($user_id);
     }
 }
