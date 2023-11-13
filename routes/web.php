838b1a1581301dc9d03daa2433b9d28b32f697fa
QziuL <lf.quinholi2@gmail.com>
2023-11-13 17:07:29 -0300

    criação de rota cadastro produto

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,10 @@ use App\Http\Controllers\EventController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\ProdutoController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [EventController::class, 'viewIndex'])->name('index');
+Route::get('/', [IndexController::class, 'viewInitial'])->name('index');
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login/auth', [LoginController::class, 'auth'])->name('login.auth');
@@ -18,3 +19,6 @@ Route::post('/register', [RegisterController::class, 'store'])->name('register.s
 Route::post('/register/agricultor', [RegisterController::class, 'storeAgricultor'])->name('register.storeAgricultor');
 
 Route::get('/index', [IndexController::class, 'show'])->name('produtos')->middleware('auth');
+
+Route::get('/registerProduto', [ProdutoController::class,'show'])->name('showProduto.cadastro')->middleware('auth');
+Route::post('/registerProduto', [ProdutoController::class,'storeProduto'])->name('storeProduto.cadastro')->middleware('auth');
