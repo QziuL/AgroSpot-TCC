730b86eb455a9cdf83820c9d0f9a42463ca98cd2
QziuL <lf.quinholi2@gmail.com>
2023-11-21 22:48:28 -0300

    add path images to produtos seeder

--- database/seeders/ProdutoSeeder.php
+++ database/seeders/ProdutoSeeder.php
@@ -32,6 +32,12 @@ class ProdutoSeeder extends Seeder
         '1',
     ];
 
+    static $image = [
+        '65b203dc703c950acc22d62aea7dd07e.png',
+        '2109a0a7bb33c819da34354703739c8a.png',
+        'f377b1d14a444edc7a89dc877c0d8044.png',
+    ];
+
     /**
      * Run the database seeds.
      */
@@ -43,6 +49,7 @@ class ProdutoSeeder extends Seeder
                 Produto::CAMPO_DESCRICAO => mb_strtoupper(self::$descricao[$i], 'UTF-8'),
                 Produto::CAMPO_CODIGO => self::$codigo[$i],
                 Produto::CAMPO_DISPONIBILIDADE => self::$disponibilidade[$i],
+                Produto::CAMPO_IMAGE => self::$image[$i],
             ]);
             $produto->save();
         }
