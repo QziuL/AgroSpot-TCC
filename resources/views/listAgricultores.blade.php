81f164b159e4f89768fef06b11074c8cc242c398
QziuL <lf.quinholi2@gmail.com>
2023-11-26 18:20:06 -0300

    view listar agricultores

--- resources/views/listAgricultores.blade.php
+++ resources/views/listAgricultores.blade.php
@@ -22,40 +22,118 @@
             </ul>
         </div>
         
-        <section>       
+        <section>
+            <div id="div-buscar">
+                <form action="{{ route('listar.agricultores') }}" method="GET">
+                    <input type="text" name="busca" id="busca" placeholder="Busque um agricultor...">
+                    <input type="submit" value="Buscar" id="btn-buscar">
+                </form>
+            </div>
+
             <div id="div-container-agricultores">
-                @php
-                    $aux2 = 0;
-                @endphp
-                @foreach ($userAgricultores as $agricultor)
-                
-                    <div class="div-agricultor">
-                        <div>
-                            <h3>{{ Str::title($agricultor[$aux]->name) }}</h3>
-                        </div>
-                        <div>
+                @if($busca)
+                    <h1>Agricultor pesquisado: {{ $busca }}</h1>
+
+                    @foreach ($userAgricultores as $userAgricultor)
+                        <div class="div-agricultor">
+                            
                             <div>
-                                <span>Localização: 
-                                    <br>
-                                    {{ Str::title($agricultores[$aux2]->cidade) }}
-                                </span>
+                                <h3>{{ Str::title($userAgricultor->name) }}</h3>
                             </div>
-                            <div>
-                                <span>Nome da propriedade: 
-                                    <br>
-                                    {{ Str::title($agricultores[$aux2]->nome_propriedade) }}
-                                </span>
-                            </div>
+                            
                         </div>
-                        <div class="div-button">
-                            <a href="#" class="button-ver-perfil">Ver perfil</a>
-                        </div>
-                    </div>
+                        @if ($busca)
+                            <a id="limparPesquisa" href="{{ route('listar.agricultores') }}">Limpar pesquisa</a>
+                        @endif
+                    @endforeach
+
+                @else
+                    <h1>Agricultores</h1>
+
                     @php
-                        $aux2++;
+                        $aux2 = 0;
                     @endphp
-                @endforeach 
+                    @foreach ($userAgricultores as $agricultor)
+
+                        <div class="div-agricultor">
+                            <div>
+                                <h3>{{ Str::title($agricultor[$aux]->name) }}</h3>
+                            </div>
+                            <div>
+                                <div>
+                                    <span>Endereço: 
+                                        <br>
+                                        {{ Str::title($agricultores[$aux2]->cidade) }}, {{$agricultores[$aux2]->cep}}
+                                    </span>
+                                </div>
+                                <br>
+                                <div>
+                                    <span>Nome da propriedade: 
+                                        <br>
+                                        {{ Str::title($agricultores[$aux2]->nome_propriedade) }}
+                                    </span>
+                                </div>
+                            </div>
+                            <div class="div-button">
+                                <a href="#" class="button-ver-perfil">Ver perfil</a>
+                            </div>
+                        </div>
+                        @php
+                            $aux2++;
+                        @endphp
+                    @endforeach 
+                @endif
+                
+                
+                @if(count($userAgricultores) == 0 && $busca)
+                   <p style="font-size: 20px; margin-bottom:10px; margin-left:15px;">
+                        O agricultor "{{$busca}}" não foi encontrado...
+                    </p>
+                   <a id="limparPesquisa" href="{{ route('listar.agricultores') }}">
+                        Limpar pesquisa
+                    </a>
+                @elseif(count($userAgricultores) == 0)
+                    <p>Não há agricultores cadastrados!</p> 
+                @endif
             </div>
         </section>
     </div>
 @endsection
+
+{{-- 
+    
+    
+@php
+    $aux2 = 0;
+@endphp
+@foreach ($userAgricultores as $agricultor)
+
+    <div class="div-agricultor">
+        <div>
+            <h3>{{ Str::title($agricultor[$aux]->name) }}</h3>
+        </div>
+        <div>
+            <div>
+                <span>Localização: 
+                    <br>
+                    {{ Str::title($agricultores[$aux2]->cidade) }}, {{$agricultores[$aux2]->cep}}
+                </span>
+            </div>
+            <br>
+            <div>
+                <span>Nome da propriedade: 
+                    <br>
+                    {{ Str::title($agricultores[$aux2]->nome_propriedade) }}
+                </span>
+            </div>
+        </div>
+        <div class="div-button">
+            <a href="#" class="button-ver-perfil">Ver perfil</a>
+        </div>
+    </div>
+    @php
+        $aux2++;
+    @endphp
+@endforeach 
+    
+--}}
