e5e895ae8308300e92d48afc67c422c3419e098f
QziuL <lf.quinholi2@gmail.com>
2023-11-26 17:45:44 -0300

    dashboard admin

--- resources/views/dashboardAdmin.blade.php
+++ resources/views/dashboardAdmin.blade.php
@@ -1,9 +1,34 @@
-@extends('layouts.main')
+@extends('layouts.mainDashboard')
 
-@section('title', 'DASHBOARD')
+@section('title', 'Dashboard - Admin')
 
 @section('content')
-    <div>
-        
+    <div id="content-main-admin">
+        <div id="div-titulo">
+            <h1>Painel - Admin</h1>
+        </div>
+        <div class="div-content">
+            <table class="table-register">
+                <thead>
+                    <tr>
+                        <th>Registrar</th>
+                        <td><a href="{{ route('showProduto.cadastro') }}">Produtos</a></td>
+                        <td><a href="{{ route('showFeira.cadastro') }}">Feiras</a></td>
+                    </tr>
+                </thead>
+            </table>
+            <table class="table-produtos">
+                <thead>
+                    <tr>
+                        <th>Produtos pendentes</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><hr></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 @endsection
