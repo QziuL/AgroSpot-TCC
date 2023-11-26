b6eca81bc6cc74e0d9b7f7dd518b1d4dacdf0bfb
QziuL <lf.quinholi2@gmail.com>
2023-11-26 18:20:16 -0300

    view list produtos

--- resources/views/agricultorListProdutos.blade.php
+++ resources/views/agricultorListProdutos.blade.php
@@ -1,24 +1,29 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Dashboard - List produtos</title>
-</head>
-<body>
-    <div id="content-main">
+@extends('layouts.mainDashboardAgricultor')
+
+@section('title', 'Dashboard - Listar Produtos')
+
+@section('content')
+    <div>
+        <h1>Seus produtos:</h1>
+    </div>
+
+    @if (count($produtos)==0)
+        <h3>Nenhum produto adicionado!!</h3>
+        <p style="font-weight: bold;">
+            Clique <a class="link-redirect" href="{{route('showAddProduto')}}">aqui</a> para adicionar.                
+        </p>
+    @else
         @foreach ($produtos as $produto)
             <div class="div-produtos-card-bd">
-                {{-- <div class="div-card-img">
+                <div class="div-card-img">
                     <img class="card-img" src="/img/produtos/{{$produto->image}}" alt="">
-                </div> --}}
+                </div>
                 <div class="card-details">
                     {{-- <span class="card-span">Tag 1</span>
                     <span class="card-span">Tag 2</span> --}}
                     <h3 class="card-nome">{{$produto->nome}}</h3>
                     <p class="descricao-produto">{{$produto->descricao}}</p>
-                    <form action="/dashboard/removeProduto/{{$produto->id}}" method="POST">
+                    <form action="/dashboard/agricultor/removeProduto/{{$produto->id}}" method="POST">
                         @csrf
                         @method('DELETE')
                         <button type="submit" value="{{$produto->id}}" id="btn_id">Remover</button>
@@ -26,6 +31,5 @@
                 </div>
             </div>
         @endforeach
-    </div>
-</body>
-</html>
+    @endif
+@endsection
